8c9ba4f99e2de52c3af876a64c92ba428cea8ef5
tetejiro <hy1733505@gmail.com>
2023-07-30 02:35:36 +0900

    <script setup>①

--- section11/set_up_version/src/App.vue
+++ section11/set_up_version/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <ScriptSetupTest></ScriptSetupTest>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ScriptSetupTest from './components/ScriptSetupTest.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ScriptSetupTest
   }
 }
 </script>
---
+++ section11/set_up_version/src/components/ScriptSetupTest.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const count = ref(0);
+
+  const increment = () => {
+    count.value++;
+  }
+
+  const decrement = () => {
+    count.value--;
+  }
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
